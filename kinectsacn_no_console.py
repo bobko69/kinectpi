70e325704fcd2eb935bc14725a6121836dec780c
bobko69 <bob.k.kojima@gmail.com>
2021-03-15 07:37:55 -0700

    code clean up

--- kinectsacn_no_console.py
+++ kinectsacn_no_console.py
@@ -6,12 +6,13 @@ import numpy as np
 from freenect import sync_get_depth as get_depth
 
 # global variables
-ipaddress = "4.3.2.1"
+ipaddress = "4.3.2.1" #wleds ipaddress
 universecount = 16
 screen_width = 48
 screen_height = 56
 
-#crontab -e @reboot python3 /home/bob/kinectsacn_no_console.py 
+#use wpa-supplicant to connect wifi to WLED
+#crontab -e @reboot python3 /home/pi/kinectpi/kinectsacn_no_console.py 
 
 def make_gamma():
     """
@@ -44,11 +45,8 @@ def make_gamma():
         _gamma[i] = a
     return _gamma
 
-
-gamma = make_gamma()
-
-
-def sendscreen2(screen, sender):
+#this function sends the sacn to wled
+def sendscreen(screen, sender):
     all = ()
     sender.manual_flush = True # turning off the automatic sending of packets
     for x in range(screen_width):
@@ -66,13 +64,12 @@ def sendscreen2(screen, sender):
     sender.flush()
     sender.manual_flush = False # keep maunal flush off as long as possible, because if it is on, the automatic
 
-
+gamma = make_gamma()
 
 def main():
     # set up sacn
     sender = sacn.sACNsender()  # provide an IP-Address to bind to if you are using Windows and want to use multicast
     sender.start()  # start the sending thread
-    # sender[1].multicast = True  # set multicast to Tru
     for i in range(universecount):
         sender.activate_output(i+1)  # start sending out data in the 1st universe
         sender[i+1].destination = ipaddress  # or provide unicast information.
@@ -81,26 +78,17 @@ def main():
 
     #set up the fpx
     pygame.init()
-    FPS = 5 #frames per second setting
+    FPS = 15 #frames per second setting
     fpsClock = pygame.time.Clock()
     disp_size = (640, 480)
 
     #set up the window
     os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (20,20)
-#    screen = pygame.display.set_mode(disp_size)
     screen = pygame.Surface(disp_size)
-#    pygame.display.set_caption('animation')
-
 
     # loop till we see the window closed
     while True: # the main game loop
-#        for event in pygame.event.get():
-#            if event.type == QUIT:
-#                sender.stop()
-#                pygame.quit()
-#                sys.exit()
-
-        depth = np.rot90(get_depth()[0]) # get the depth readinngs from the camera
+        depth = np.rot90(get_depth()[0]) # get the depth readings from the kinect
         pixels = gamma[depth] # the colour pixels are the depth readings overlayed onto the gamma table
         temp_surface = pygame.Surface(disp_size)
         pygame.surfarray.blit_array(temp_surface, pixels)
@@ -109,10 +97,8 @@ def main():
         cropped.blit(temp_surface,(0,0), (10,0,640,480))
         led_surface = pygame.Surface((48,56))
         led_surface = pygame.transform.scale(cropped, (48,56))
-#        screen.blit(led_surface, (0,0))
-        # pygame.display.flip()
 
-        sendscreen2(led_surface, sender)
+        sendscreen(led_surface, sender)
         fpsClock.tick(FPS)
 
 
